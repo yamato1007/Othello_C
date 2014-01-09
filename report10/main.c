1d06d48cc11d7139f70cbcc9ba35836de55e1506
k13082kk <k13082kk@pck13082kk.local>
2014-01-09 10:12:48 +0900

    Initial Commit

---
+++ report10/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  report10
+//
+//  Created by k13082kk on 2014/01/09.
+//  Copyright (c) 2014年 k13082kk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
