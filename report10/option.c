31bf9ca36cb047b952db0b4bedd3e04576a198df
yamato_tanaka <a.niger.333141592653589793@gmail.com>
2016-01-16 18:41:52 +0900

    PDF修正

--- report10/option.c
+++
@@ -1,9 +0,0 @@
-//
-//  option.c
-//  othello
-//
-//  Created by k13082kk on 2014/01/27.
-//  Copyright (c) 2014年 k13082kk. All rights reserved.
-//
-
-#include <stdio.h>
--- report10/othelo_do.c
+++
@@ -1,9 +0,0 @@
-//
-//  othelo_do.c
-//  othello
-//
-//  Created by k13082kk on 2014/01/27.
-//  Copyright (c) 2014年 k13082kk. All rights reserved.
-//
-
-#include <stdio.h>
